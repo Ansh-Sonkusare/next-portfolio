3366a3e28e06faa2b6c1cc9ddcd9d643ee6a523e
Ansh-Sonkusare <sonkusare.satish12@gmail.com>
2023-06-25 12:47:20 +0530

    test-action

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,28 +1,30 @@
 'use server'
 
 export async function addItem(data:FormData) {
-    console.log(process.env.hookUrl);
+    console.log(data);
     
-    const user = {
-        name:data.get("name") as string,
-        email:data.get("email")as string ,
-        message:data.get("message")as string
-    } 
-    const params = {
-        username: "Email From Website",
-        content:"<@651365234459541514>",
-        "embeds": [{
-            "title": "Message!",
-            "description": `**Sent by**: ${user.name}\n**Email**: ${user.email}\n**Message**: ${user.message}`
-          }]
-      }
+//     console.log(process.env.hookUrl);
+    
+//     const user = {
+//         name:data.get("name") as string,
+//         email:data.get("email")as string ,
+//         message:data.get("message")as string
+//     } 
+//     const params = {
+//         username: "Email From Website",
+//         content:"<@651365234459541514>",
+//         "embeds": [{
+//             "title": "Message!",
+//             "description": `**Sent by**: ${user.name}\n**Email**: ${user.email}\n**Message**: ${user.message}`
+//           }]
+//       }
   
-      fetch(process.env.hookUrl, {
-          method: "POST",
-          headers: {
-              'Content-type': 'application/json'
-          },
-          body: JSON.stringify(params)
-      })
+//       fetch(process.env.hookUrl, {
+//           method: "POST",
+//           headers: {
+//               'Content-type': 'application/json'
+//           },
+//           body: JSON.stringify(params)
+//       })
     
   }
